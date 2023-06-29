d8e60a2a94f1c85a4e942f4b4313e98e4d071f49
Andrii Laskovets <clanzy8@gmail.com>
2023-06-29 20:22:46 +0300

    Update

---
+++ 64_dp.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int minPathSum(vector<vector<int> > &grid)
+{
+	for (int i = 1; i < grid.size(); ++i) {
+		grid[i][0] += grid[i - 1][0];
+	}
+	for (int j = 1; j < grid[0].size(); ++j) {
+		grid[0][j] += grid[0][j - 1];
+	}
+
+	for (int i = 1; i < grid.size(); ++i) {
+		for (int j = 1; j < grid[i].size(); ++j) {
+			grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+		}
+	}
+	return grid.back().back();
+}
+
+int main()
+{
+	//vector<vector<int> > matrix = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+	vector<vector<int> > matrix = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+	cout << minPathSum(matrix) << '\n';
+
+	return 0;
+}
