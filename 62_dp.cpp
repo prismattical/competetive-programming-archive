f9712fba9756e6a9b57c6ad89ff574b5cb2861dc
Andrii Laskovets <clanzy8@gmail.com>
2023-06-29 20:06:06 +0300

    Update

---
+++ 62_dp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int uniquePaths(int m, int n)
+{
+	vector<vector<int> > matrix(m + 1, vector<int>(n + 1, 0));
+	matrix[1][1] = 1;
+	for (int i = 1; i < m + 1; ++i) {
+		for (int j = 1; j < n + 1; ++j) {
+			matrix[i][j] += matrix[i - 1][j] + matrix[i][j - 1];
+		}
+	}
+	return matrix[m][n];
+}
+
+int main()
+{
+	int m, n;
+	cin >> m >> n;
+
+	cout << uniquePaths(m, n) << '\n';
+
+	return 0;
+}
