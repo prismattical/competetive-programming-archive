b05628a63efa6328b4e6d2ac0da60786e35c9fab
Andrii Laskovets <prismattical@gmail.com>
2024-01-18 19:48:10 +0200

    Update

---
+++ 128_array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestConsecutive(vector<int> &nums)
+{
+	if (nums.size() == 0)
+	{
+		return 0;
+	}
+	set<int> memo;
+	for (auto i : nums)
+	{
+		memo.insert(i);
+	}
+	int k = 1;
+	int max_so_far = k;
+	auto it = memo.begin();
+	int prev = *it;
+	++it;
+	for (; it != memo.end(); ++it)
+	{
+		if (*it == prev + 1)
+		{
+			++k;
+			max_so_far = max(k, max_so_far);
+		} else if (*it != prev + 1 && *it != prev)
+		{
+			k = 1;
+		}
+		prev = *it;
+	}
+    return max_so_far;
+}
+
+int main()
+{
+}
---
+++ 20_stack.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s)
+{
+	vector<char> stack;
+	for (char ch : s)
+	{
+		if (ch == '{' || ch == '(' || ch == '[')
+		{
+			stack.push_back(ch);
+		}
+		if (ch == '}' || ch == ')' || ch == ']')
+		{
+			if (stack.empty())
+			{
+				return false;
+			}
+			char ch2 = stack.back();
+			stack.pop_back();
+
+			if ((ch2 == '{' && ch == '}') || (ch2 == '(' && ch == ')') ||
+			    (ch2 == '[' && ch == ']'))
+			{
+				continue;
+			}
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+}
